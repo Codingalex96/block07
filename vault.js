5f3ed57f0a618d8fddb85c0a33923d525b6c1bf0
Alex Harrison <alexharrison96@yahoo.com>
2024-07-30 20:38:19 -0500

    combo.html

--- vault.js
+++
@@ -1,9 +0,0 @@
-const msg = "You have recieved this message becayse you have been chosen to open an important vault. Here is the secret combination" 
-// combo is equal to 10 so had number one variable 20 - 10 to = 10//
-const num1 = 20 - 10
-
-const num2 = 20 * 2
-
-const num3 = 40 - 1
-
-alert(`${msg} ${num1} - ${num2} - ${num3}`);
