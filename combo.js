e224bc0031650895385018bed9103c2f6f2c845b
Alex Harrison <alexharrison96@yahoo.com>
2024-07-30 20:47:33 -0500

    combo.js

--- combo.js
+++ combo.js
@@ -1,5 +1,11 @@
 const msg = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+// combo 1 is 10 so i did 20-10 for 10 //
 const num1 = 20 - 10;
+
+
 const num2 = 20 * 2;
+
+// here i had the alert show the message then the corresponding numbers with dashes in between to creat spaces
 const num3 = 40 - 1;
 alert(`${msg} ${num1} - ${num2} - ${num3}`);
